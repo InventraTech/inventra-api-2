4a4a580d5f74d85380a405edec34c9d0a56bbe27
dvarakaki <davi.arakaki@hotmail.com>
2026-07-08 09:01:54 -0300

    feat: Iniciando projeto da api

---
+++ src/main/java/com/inventra/api/InventraApiApplication.java
@@ -0,0 +1,13 @@
+package com.inventra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventra/api/InventraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventra.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
